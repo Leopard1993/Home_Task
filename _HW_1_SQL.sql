60dab9434a520f1c0b210963648eb906ccd2f19b
Leopard1993 <bakurin1993@ryandex.ru>
2022-01-07 16:47:46 +0300

    redaction_HW_SQL_Postman

--- _HW_1_SQL.sql
+++ _HW_1_SQL.sql
@@ -76,7 +76,7 @@ select min(id) from students;
 --29. ¬ывести максимальный.
 select max(id) from students;
 --30. ¬ывести количество пользователей
-select count(name) from students;
+select count(id) from students;
 --31. ¬ывести id пользовател€, им€, дату создани€ пользовател€. ќтсортировать по пор€дку возрастани€ даты добавлени€ пользоватлел€.
 select id, name, created_on from students
 	order by created_on;
